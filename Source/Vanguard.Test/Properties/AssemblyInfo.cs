e2b52637d9f677da797b4547a4dd1e61dbba99d0
Reiv_cp <SND\Reiv_cp>
2015-05-11 20:39:13 +0000

    Breaking changes: - Renamed Guard.ValueIsNotNull to Guard.ValueNotNull. - Renamed Guard.ValueIsNotNullOrEmpty to Guard.ValueNotNullOrEmpty. - Renamed Guard.ValueIsInRange to Guard.ValueInRange.

--- Source/Vanguard.Test/Properties/AssemblyInfo.cs
+++ Source/Vanguard.Test/Properties/AssemblyInfo.cs
@@ -8,8 +8,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Puur Software 2015")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3b9cb5c5-9eff-4951-944c-bbc916114101")]
-[assembly: AssemblyVersion("2.1.2")]
-[assembly: AssemblyFileVersion("2.1.2")]
+[assembly: AssemblyVersion("2.2.0")]
+[assembly: AssemblyFileVersion("2.2.0")]
 
 #if DEBUG
 [assembly: AssemblyConfiguration("Debug")]
